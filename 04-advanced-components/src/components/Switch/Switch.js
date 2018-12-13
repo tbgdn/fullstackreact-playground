5ac06dfcbc49c37a32608bbeb943711e40114cca
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2018-12-14 00:29:01 +0200

    Deleted the implemented Switch components and replaced with a single one.

--- 04-advanced-components/src/components/Switch/Switch.js
+++ 04-advanced-components/src/components/Switch/Switch.js
@@ -1,74 +1,42 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import styles from "./Switch.css";
 
-import { default as Switch1 } from './steps/Switch1';
-import { default as Switch2 } from './steps/Switch2';
-import { default as Switch3 } from './steps/Switch3';
-import { default as Switch4 } from './steps/Switch4';
-import { default as Switch5 } from './steps/Switch5';
-import { default as Switch6 } from './steps/Switch6';
-
-// import ActiveSpan from '../ActiveSpan/ActiveSpan';
-// import styles from './Switch.css';
-
-const CREDITCARD = 'Creditcard';
-const BTC = 'Bitcoin';
+const CREDIT_CARD = "CreditCard";
+const BITCOIN = "Bitcoin";
 
 class Switch extends React.Component {
-  // getInitialState() {
-  //   return {
-  //     payMethod: BTC
-  //   };
-  // },
-
-  // select: function(choice) {
-  //   const that = this;
-  //   return function(evt) {
-  //     that.setState({
-  //       payMethod: choice
-  //     });
-  //   };
-  // },
-
-  // _renderChoice: function(choice) {
-  //   return (
-  //     <ActiveSpan onClick={this.select(choice)}
-  //                 active={this.state.payMethod === choice}
-  //                 activeClass={styles.active}
-  //                 className={styles.choice}
-  //                 label={choice} />
-  //   )
-  // },
-
-  // render: function() {
-  //   return (
-  //     <div className={styles.switch}>
-  //       <div className={styles.diamond} />
-  //       {this._renderChoice(CREDITCARD)}
-  //       {this._renderChoice(BTC)}
-  //     </div>
-  //   )
-  // }
-  render() {
-    const presentStep = (title, Step) => (
-      <div>
-        <h2>{title}</h2>
-        <Step />
-      </div>
-    );
-
-    return (
-      <div>
-        {presentStep('Switch1', Switch1)}
-        {presentStep('Switch2', Switch2)}
-        {presentStep('Switch3', Switch3)}
-        {presentStep('Switch4', Switch4)}
-        {presentStep('Switch5', Switch5)}
-        {presentStep('Switch6', Switch6)}
-      </div>
-    );
-  }
+    state = {
+        paymentMethod: BITCOIN
+    };
+    renderChoice = (choice) => {
+        const cssClasses = [];
+        if (this.state.paymentMethod === choice){
+            cssClasses.push(styles.active);
+        }
+        return (
+            <div className={cssClasses}
+                onClick={this.handleChoiceOnClick(choice)}
+            >
+                {choice}
+            </div>
+        );
+    };
+    handleChoiceOnClick = (choice) => {
+        return () => {
+            this.setState({
+                paymentMethod: choice
+            })
+        };
+    };
+    render(){
+        return (
+            <div className="switch">
+                {this.renderChoice(CREDIT_CARD)}
+                {this.renderChoice(BITCOIN)}
+                Pay with: {this.state.paymentMethod}
+            </div>
+        );
+    }
 }
 
 export default Switch;
