8a327b76be79b78f0c79b914a9149d94b76bac64
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-01-07 01:46:31 +0200

    Started the Field component.

---
+++ 05-forms/src/Field.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+import classnames from "classnames";
+
+class Field extends Component{
+    static propTypes = {
+        placeholder: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        iconClass: PropTypes.string,
+        value: PropTypes.string,
+        validate: PropTypes.func,
+        onChange: PropTypes.func.isRequired,
+
+    };
+    state = {
+        value: this.props.value,
+        error: false
+    };
+    handleInputChange = (event) => {
+        let name = this.props.name;
+        let value = event.target.value;
+        let error = this.props.validate ? this.props.validate(value) : false;
+        this.setState(() => ({
+            value,
+            error
+        }));
+        this.props.onChange({name, value, error});
+    };
+    render(){
+        let iconClasses = classnames("icon", {
+            [this.props.iconClass]: this.props.iconClass,
+            "ellipsis": !this.props.iconClass,
+            "horizontal": !this.props.iconClass,
+        });
+        return (
+            <div className="field">
+                <div className="ui left icon input">
+                    <input type="text"
+                           placeholder={this.props.placeholder}
+                           name={this.props.name}
+                           value={this.state.value}
+                           onChange={this.handleInputChange}
+                    />
+                    <i className={iconClasses}></i>
+                </div>
+                {
+                    this.state.error ? (
+                        <div className="ui red inverted segment">
+                            <p>{this.state.error}</p>
+                        </div>
+                    ) : <span/>
+                }
+            </div>
+        );
+    };
+}
+
+export default Field;
--- 05-forms/src/Inputs.jsx
+++ 05-forms/src/Inputs.jsx
@@ -1,36 +1,100 @@
 import React, {Component} from "react";
+import {isEmail} from "validator";
+import Field from "./Field";
 
 class Inputs extends Component{
     state = {
-        name: "",
-        names: []
+        fields: {
+            name: "",
+            email: ""
+        },
+        people: []
     };
     handleSubmit = (event) => {
-        let names = [...this.state.names, this.state.name];
-        this.setState({names: names, name: ""});
+        this.setState((state) => {
+            let person = this.state.fields;
+            let fieldErrors = this.validate(person);
+            if (Object.keys(fieldErrors).length){
+                return {fieldErrors};
+            }else{
+                return {
+                    people: [...state.people, state.fields],
+                    fields: {
+                        name: "",
+                        email: ""
+                    },
+                    fieldErrors: {}
+                };
+            }
+        });
         event.preventDefault();
     };
-    handleNameChange = (event) => {
-        this.setState({name: event.target.value});
+    handleInputChange = (name, value, error) => {
+        this.setState((state) => {
+            let fields = Object.assign({}, state.fields);
+            fields[name] = value;
+            return fields;
+        });
+    };
+    isFormValid = () => {
+        return !this.validateName(this.state.fields.name) &&
+            !this.validateEmail(this.state.fields.email);
+    };
+    validateName = (name) => {
+        if (name){
+            return false;
+        }else{
+            return "Name is required";
+        }
+    };
+    validateEmail = (email) => {
+        if (!email){
+            return "Email is required";
+        }
+        if (!isEmail(email)){
+            return "Email must be valid"
+        }
+        return false;
     };
     render(){
         return (
             <div className="ui segment">
                 <h2 className="ui header">Sign Up Sheet</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="ui action input">
-                        <input placeholder="Name"
-                               type="text"
-                               value={this.state.name}
-                               onChange={this.handleNameChange}/>
-                        <button className="ui primary button">Add</button>
-                    </div>
+                <form onSubmit={this.handleSubmit} className="ui form">
+                    <Field
+                        placeholder="Name"
+                        iconClass="users"
+                        name="name"
+                        value={this.state.fields.name}
+                        validate={this.validateName}
+                        onChange={this.handleInputChange}
+                    />
+                    <Field
+                        placeholder="Email"
+                        iconClass="envelope"
+                        name="email"
+                        value={this.state.fields.name}
+                        validate={this.validateEmail}
+                        onChange={this.handleInputChange}
+                    />
+                    <button className="ui button positive"
+                            disabled={!this.isFormValid()}
+                            type="submit">Add</button>
                 </form>
-                <div>
-                    <h3>Names</h3>
+                <div className="ui segment">
+                    <h3 className="ui header">
+                        <i className="users icon"></i>
+                        <div className="content">
+                            People
+                        </div>
+                    </h3>
                     <div className="ui list">
                         {
-                            this.state.names.map((name, index) => (<div key={index} className="item">{name}</div>))
+                            this.state.people.map((p) => (
+                                <div key={p.name + p.email} className="item">
+                                    {p.name}({p.email})
+                                </div>
+                            ))
                         }
                     </div>
                 </div>
